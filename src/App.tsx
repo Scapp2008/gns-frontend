f442529fd6cb789c2878f4f2856cfc124402659d
asaje379 <salemaffa@gmail.com>
2024-01-27 20:38:38 +0100

    b-a-ba de react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [value, setValue] = useState(0);
+
+  const increment = () => {
+    setValue(value + 1);
+  };
+
+  const decrement = () => {
+    setValue(value - 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="p-10">
+      <div className="mb-4">Valeur: {value}</div>
+      <div className="flex gap-4">
+        <button
+          onClick={increment}
+          className="bg-blue-500 hover:bg-blue-700 text-white rounded px-12 py-3">
+          Incrémenter
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={decrement}
+          className="bg-red-500 hover:bg-red-700 text-white rounded px-12 py-3">
+          Décrémenter
+        </button>
+        {value}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
